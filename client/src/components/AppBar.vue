76dcb1a7f1b06ea93787a0b7fceb4fa56660a099
personal <hroudaadam@gmail.com>
2020-08-31 09:15:40 +0200

    priprava FE

---
+++ client/src/components/AppBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+      <b-navbar-brand href="#">Adam Hrouda</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Úvod</b-nav-item>
+          <b-nav-item href="#">Blog</b-nav-item>
+          <b-nav-item href="#" disabled>Programování</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              Uživatel
+            </template>
+            <b-dropdown-item href="#">Profil</b-dropdown-item>
+            <b-dropdown-item href="#">Odhlásit</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppBar',
+    components: {
+
+    }
+}
+</script>
